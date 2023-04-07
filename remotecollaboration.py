c80ae5a083448e29efbb12cff5915cb2b9a04bcb
Evan Murray <evan@auraaudio.io>
2023-04-07 01:05:19 -0400

    Remove prints

--- remotecollaboration.py
+++ remotecollaboration.py
@@ -7,15 +7,9 @@ import git
 # Find the Reaper project inside this repo
 project = reapy.Project('remotecollaboration')
 
-reapy.print(project)
-
 # Find the Git repo inside the working directory (same as project path)
 repo = git.Repo(project.path)
-
-reapy.print(repo)
 
 # Fetch the origin
 origin = repo.remotes.origin
 origin.fetch()
-
-reapy.print(origin.refs)
