4b33ec7ca6d9c62e0da22029b8ab3185fd14707a
Evan Murray <evan@auraaudio.io>
2023-04-07 06:12:22 -0400

    Add ReaImGui
    Create an ImGui-like window which allows Reaper to interact with GitHub

--- remotecollaboration.py
+++ remotecollaboration.py
@@ -1,6 +1,8 @@
 # This script allows remote collaboration for Reaper projects
 # via GitHub
-
+import sys
+sys.path.append(RPR_GetResourcePath() + '/Scripts/ReaTeam Extensions/API')
+import imgui_python
 import reapy
 import git
 
@@ -10,6 +12,26 @@ project = reapy.Project('remotecollaboration')
 # Find the Git repo inside the working directory (same as project path)
 repo = git.Repo(project.path)
 
-# Fetch the origin
+# Get the origin
 origin = repo.remotes.origin
-origin.fetch()
+
+def init():
+  global ctx
+  ctx = imgui_python.ImGui_CreateContext('GitHub Reaper')
+  loop()
+
+def loop():
+  imgui_python.ImGui_SetNextWindowSize(ctx, 700, 500, imgui_python.ImGui_Cond_FirstUseEver())
+  visible, open = imgui_python.ImGui_Begin(ctx, 'GitHub Reaper', True)
+
+  if visible:
+    if imgui_python.ImGui_Button(ctx, 'Fetch Origin'):
+      origin.fetch()
+      reapy.print('Successfully fetched:', origin.name,'at:',origin.url,'branches:',origin.refs)
+
+    imgui_python.ImGui_End(ctx)
+
+  if open:
+    RPR_defer('loop()')
+
+RPR_defer('init()')
