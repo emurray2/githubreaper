28c39288d88f4e651fd11c8c0067d30c49ee6e7d
Evan Murray <evan@auraaudio.io>
2023-04-08 10:32:31 -0400

    Don't add on publishing new branch

--- remotecollaboration.py
+++ remotecollaboration.py
@@ -129,9 +129,6 @@ def loop():
           if commit_message[0] == '':
             reapy.show_message_box("Please enter text for commit message", "Commit Failed")
           if new_branch_name[0] != '' and commit_message != '':
-            files = repo.git.diff(None, name_only=True)
-            for f in files.split('\n'):
-              repo.git.add(f)
             repo.index.commit(commit_message[0])
             origin.push(new_branch_name[0])
           else:
