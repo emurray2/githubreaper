9b18c755a1cb50abe491a16897c3b90edaf8a07b
Evan Murray <evan@auraaudio.io>
2023-04-08 22:15:57 -0400

    Make steps even more clear

--- githubreaper.py
+++ githubreaper.py
@@ -37,7 +37,7 @@ git_ssh_identity_file = os.path.expanduser('~/.ssh/id_ed25519')
 git_ssh_cmd = 'ssh -i %s' % git_ssh_identity_file
 # Get the origin
 origin = repo.remotes.origin
-# Switch to url of the repo you wish to use
+# Switch to url of the repo you wish to use (has to be in SSH format like this--no https)
 repo_url = 'git@github.com:emurray2/reapergithub.git'
 origin.set_url(repo_url)
 
