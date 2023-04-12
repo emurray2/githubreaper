0bf08c6067bf42635192aaba1dd552e299e02248
Evan Murray <evan@auraaudio.io>
2023-04-12 05:23:32 -0400

    Comments

--- conflictgui.py
+++ conflictgui.py
@@ -33,9 +33,9 @@ def init():
     global ctx
     # List of dictionaries containing the buttons states for each row
     global row_states
-    # Last button the user clicked
+    # Reference containing the last button id the user pressed
     global last_button
-    # Last row the user interacted with
+    # Reference containing the last row number the user interacted with
     global last_row
     # List of conflicts
     global conflict_list
@@ -61,40 +61,53 @@ def createConflictTable(conflicts):
     num_columns = 3
 
     imgui_python.ImGui_BeginTable(ctx, 'Conflict Table', num_columns)
+
+    # Create the table header
     imgui_python.ImGui_TableSetupColumn(ctx, '')
     imgui_python.ImGui_TableSetupColumn(ctx, 'Side #1')
     imgui_python.ImGui_TableSetupColumn(ctx, 'Side #2')
     imgui_python.ImGui_TableHeadersRow(ctx)
 
+    # Create rows with each conflict name and the corresponding buttons for each side
     addConflicts(conflicts, num_columns-1)
 
     imgui_python.ImGui_EndTable(ctx)
 
 def addConflicts(conflicts, num_buttons: int):
+    # Render (loop through) each row
     for row in range(0, len(conflicts)):
         imgui_python.ImGui_TableNextColumn(ctx)
         imgui_python.ImGui_Text(ctx, conflicts[row])
         addButtons(row, num_buttons)
 
 def addButtons(row_number: int, num_buttons: int):
+    # Create a dictionary for the row state if it doesn't exist
     if not 0 <= row_number < len(row_states):
         row_states.append({})
 
+    # Get the reference dictionary for the current row being rendered
     button_states = row_states[row_number]
 
+    # Loop through the buttons in this row
     for i in range(0, num_buttons):
         imgui_python.ImGui_TableNextColumn(ctx)
         button_id = str(row_number) + str(i)
 
+        # If this button doesn't have a state already, add it to the dictionary
         if button_id not in button_states:
             button_states[button_id] = False
 
+        # If this button is clicked, do the following:
         if imgui_python.ImGui_RadioButton(ctx, '##radio_table_' + str(button_id), button_states[button_id]):
+            # Get the row number and id of the clicked button and them in their respective references
             last_row[0] = row_number
             last_button[0] = button_id
+
+        # Deactivate the other buttons in this row
         elif last_row[0] == row_number:
             button_states[button_id] = False
 
+        # Keep the state of the last button pressed
         button_states[last_button[0]] = True
 
 RPR_defer('init()')
